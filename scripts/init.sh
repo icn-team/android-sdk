d76fcfdca352b17729b3a0d35f7febf131b620f2
Angelo Mantellini <manangel@cisco.com>
2019-10-07 11:16:57 +0200

    correct init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -93,8 +93,7 @@ fi
 
 if [ ! -d hicn ]; then
 	echo "libhicn not found"
-	#git clone https://gerrit.fd.io/r/hicn
-	git clone https://wwwin-github.cisco.com/icn-team/hicn.git -b android
+	git clone https://gerrit.fd.io/r/hicn
 fi
 
 if [ ! -d curl ]; then
