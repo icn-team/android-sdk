748a2ae883316f45543ee756ca363ba768d54343
Angelo Mantellini (manangel) <manangel@cisco.com>
2019-10-23 21:20:34 +0200

    Update WifiIPv4PreferencesFragment.java

--- HicnForwarderAndroid/app/src/main/java/com/cisco/hicn/forwarder/preferences/WifiIPv4PreferencesFragment.java
+++ HicnForwarderAndroid/app/src/main/java/com/cisco/hicn/forwarder/preferences/WifiIPv4PreferencesFragment.java
@@ -50,7 +50,7 @@ public class WifiIPv4PreferencesFragment extends PreferenceFragmentCompat {
 
                 int sourcePort = Integer.parseInt((String) newValue);
 
-                if (sourcePort < 0 && sourcePort > 65535)
+                if (sourcePort < 0 || sourcePort > 65535)
                     return false;
                 String nextHopIp = sharedPreferences.getString(getString(R.string.wifi_nexthop_ipv4_key), getString(R.string.default_wifi_nexthop_ipv4));
                 int nextHopPort = Integer.parseInt(sharedPreferences.getString(getString(R.string.wifi_nexthop_port_ipv4_key), getString(R.string.default_wifi_nexthop_port_ipv4)));
@@ -87,7 +87,7 @@ public class WifiIPv4PreferencesFragment extends PreferenceFragmentCompat {
 
                 int nextHopPort = Integer.parseInt((String) newValue);
 
-                if (nextHopPort < 0 && nextHopPort > 65535)
+                if (nextHopPort < 0 || nextHopPort > 65535)
                     return false;
                 int sourcePort = Integer.parseInt(sharedPreferences.getString(getString(R.string.wifi_source_port_ipv4_key), getString(R.string.default_wifi_source_port_ipv4)));
 
