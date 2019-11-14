11278a4ea0f76284a70ecb4a68bb3e1a0ab59bef
Angelo Mantellini <manangel@cisco.com>
2019-11-14 09:52:03 +0100

    Update build_apk.sh

--- ci/build_apk.sh
+++ ci/build_apk.sh
@@ -22,8 +22,6 @@ export ANDROID_NDK_HOME=/opt/android-sdk-linux/ndk-bundle/
 if [ ! -d /src/viper ]; then
 	git clone -b viper/master https://gerrit.fd.io/r/cicn /src/viper
 fi
-
-export ANDROID_ARCH=arm64_v8a
 export OS=$(uname | tr '[:upper:]' '[:lower:]')
 export ARCH=$(uname -m)
 export ANDROID_HOME=${ANDROID_HOME} 
@@ -33,6 +31,8 @@ export ANDROID_NDK_ROOT=${ANDROID_HOME}/ndk-bundle
 export ANDROID_SDK_ROOT=${ANDROID_HOME}
 export ANDROID_API_VERSION=android-28
 export PATH=$PATH:${ANDROID_HOME}/tools:${JAVA_HOME}/bin
+
+export ANDROID_ARCH=arm64_v8a
 export DISTILLARY_INSTALLATION_PATH=/usr_aarch64/
 mkdir -p /build_aarch64/viper
 cd /build_aarch64/viper
@@ -44,6 +44,20 @@ ${QT_HOME}/$QT_VERSION/android_${ANDROID_ARCH}/bin/androiddeployqt --output hicn
 
 cp /build_aarch64/viper/hicn-viper-arm64_v8a//build/outputs/apk/hicn-viper-arm64_v8a-release-signed.apk /hicn
 mv /hicn/hicn-viper-arm64_v8a-release-signed.apk /hicn/viper-arm64.apk
+
+export DISTILLARY_INSTALLATION_PATH=/usr_aarch64/
+export ANDROID_ARCH=x86
+mkdir -p /build_i686/viper
+cd /build_i686/viper
+${QT_HOME}/$QT_VERSION/android_${ANDROID_ARCH}/bin/qmake -r -spec android-clang /src/viper/viper.pro  "TRANSPORT_LIBRARY = HICNET"
+make
+make install INSTALL_ROOT=hicn-viper-${ANDROID_ARCH}
+${QT_HOME}/$QT_VERSION/android_${ANDROID_ARCH}/bin/androiddeployqt --output hicn-viper-${ANDROID_ARCH} --verbose --input android-libviper.so-deployment-settings.json --gradle --android-platform ${ANDROID_NDK_PLATFORM} --stacktrace --release --target ${ANDROID_NDK_PLATFORM} --release \
+--sign /src/viper/android/viper.keystore viper --storepass icn_viper
+
+cp /build_i686/viper/hicn-viper-x86//build/outputs/apk/hicn-viper-x86-release-signed.apk /hicn
+mv /hicn/hicn-viper-x86-release-signed.apk /hicn/viper-x86.apk
+
 
 ln -sf /usr_aarch64 /hicn
 ln -sf /usr_i686 /hicn
