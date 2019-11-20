84052a9da150e1297343c6252c4e33dc5e390e53
Angelo Mantellini <angelo.mantellini@cisco.com>
2019-11-20 22:16:06 +0100

    correct build

--- HicnForwarderAndroid/app/src/main/jni/hproxy-wrapper.cpp
+++ HicnForwarderAndroid/app/src/main/jni/hproxy-wrapper.cpp
@@ -73,6 +73,7 @@ createTunDeviceWrap(JNIEnv *env, jobject instance, const char *vpn_address, uint
 extern "C" int createTunDevice(const char *vpn_address, uint16_t prefix_length,
                     const char *route_address,
                     uint16_t route_prefix_length, const char *dns) {
+#ifdef ENABLE_HPROXY
     if (!_env || !_instance) {
         __android_log_print(ANDROID_LOG_ERROR, "HProxyWrap",
                             "Call createTunDevice, but _env and _instance variables are not initialized.");
@@ -81,6 +82,9 @@ extern "C" int createTunDevice(const char *vpn_address, uint16_t prefix_length,
 
     return createTunDeviceWrap(_env, *_instance, vpn_address, prefix_length, route_address,
                                route_prefix_length, dns);
+#else
+    return 0;
+#endif
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
@@ -99,126 +103,3 @@ Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_stop(JNIEnv *env, jobject in
     proxy->stop();
 #endif
 }
-
-
-# if 0
-static jfieldID _get_self_id(JNIEnv *env, jobject this_obj) {
-    static int init = 0;
-    static jfieldID fid_self_ptr;
-
-    if (!init) {
-        jclass this_class = env->GetObjectClass(this_obj);
-        fid_self_ptr = env->GetFieldID(this_class, "self_ptr", "J");
-    }
-
-    return fid_self_ptr;
-}
-
-static HicnProxy *_get_self(JNIEnv *env, jobject this_obj) {
-    jlong self_ptr = env->GetLongField(this_obj, _get_self_id(env, this_obj));
-    return (HicnProxy *) &self_ptr;
-}
-
-static void _set_self(JNIEnv *env, jobject this_obj, HicnProxy *self) {
-    jlong self_ptr = *(jlong *) &self;
-    env->SetLongField(this_obj, _get_self_id(env, this_obj), self_ptr);
-}
-
-
-
-
-
-
-
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_stop(JNIEnv *env, jobject instance) {
-//    HicnProxy *proxy = _get_self(env, instance);
-//    proxy->stop();
-}
-
-#ifdef ENABLE_HPROXY
-static jint _add_connector(JNIEnv *env,
-                           jobject instance, int connector_id,
-                           hproxy::connectors::ConnectorConfig &config) {
-#if 0
-    // Create the new connector
-    HicnProxy *proxy = _get_self(env, instance);
-#endif
-    return jint(proxy->createConnector(connector_id, config));
-}
-
-#endif
-
-extern "C" JNIEXPORT jint JNICALL
-Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_addUdpTunnelConnectorInternal(JNIEnv *env,
-                                                                                  jobject instance,
-                                                                                  jint connector_id,
-                                                                                  jstring remote_address,
-                                                                                  jstring remote_port) {
-#ifdef ENABLE_HPROXY
-    // Create connector configuration object
-    const char *_remote_address = env->GetStringUTFChars(remote_address, 0);
-    const char *_remote_port = env->GetStringUTFChars(remote_port, 0);
-    hproxy::connectors::ConnectorConfig config(
-            ConfigConnectorType::UDP_TUNNEL_CONNECTOR, _remote_address, _remote_port, "0.0.0.0");
-    return _add_connector(env, instance, connector_id, config);
-#else
-    return 0;
-#endif
-}
-
-extern "C" JNIEXPORT jint JNICALL
-Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_addVpnConnectorInternal(JNIEnv *env,
-                                                                            jobject instance,
-                                                                            jint connector_id,
-                                                                            jint fd) {
-#ifdef ENABLE_HPROXY
-    // Create connector configuration object
-    hproxy::connectors::ConnectorConfig config(
-            ConfigConnectorType::ANDROID_VPN_CONNECTOR, fd);
-    return _add_connector(env, instance, connector_id, config);
-#else
-    return 0;
-#endif
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_linkTunToConnector(JNIEnv *env,
-                                                                       jobject instance,
-                                                                       jint connector_id_0,
-                                                                       jint connector_id_1) {
-#if 0
-    HicnProxy *proxy = _get_self(env, instance);
-#endif
-#ifdef ENABLE_HPROXY
-    hproxy::DemuxInfo demuxInfo(0, 0);
-    proxy->linkFlowToConnector(Connector::Id(connector_id_0), demuxInfo,
-                               Connector::Id(connector_id_1));
-#endif
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_linkConnectorsInternal(JNIEnv *env,
-                                                                           jobject instance,
-                                                                           jint connector_id_0,
-                                                                           jint connector_id_1) {
-#if 0
-    HicnProxy *proxy = _get_self(env, instance);
-#endif
-#ifdef ENABLE_HPROXY
-    proxy->linkConnector(
-            Connector::Id(connector_id_0), Connector::Id(connector_id_1));
-#endif
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_cisco_hicn_forwarder_supportlibrary_HProxy_removeConnectorInternal(JNIEnv *env,
-                                                                            jobject instance,
-                                                                            jint connector_id) {
-    // Do remove connector from the proxy
-}
-
-
-
-#endif
