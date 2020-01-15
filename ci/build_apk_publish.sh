7f8e81db9ceaa7f6070d69a611c6f62d6c33ec3b
Angelo Mantellini <angelo.mantellini@cisco.com>
2020-01-15 16:59:06 +0100

    add version to hicn tools

--- ci/build_apk_publish.sh
+++ ci/build_apk_publish.sh
@@ -69,6 +69,7 @@ export ANDROID_API_VERSION=android-28
 export PATH=$PATH:${ANDROID_HOME}/tools:${JAVA_HOME}/bin
 
 sed -i -e "s/android:versionCode=\"9\"/android:versionCode=\"$VERSION_CODE\"/g" /src/viper/android/AndroidManifest.xml
+sed -i -e "s/android:targetSdkVersion=\"26\"/android:targetSdkVersion=\"28\"/g" /src/viper/android/AndroidManifest.xml
 export ANDROID_ARCH=arm64_v8a
 export DISTILLARY_INSTALLATION_PATH=/usr_aarch64/
 export QT_HOST_PREFIX=/qt/Qt/$QT_VERSION/android_${ANDROID_ARCH}
