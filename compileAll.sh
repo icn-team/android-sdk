a2af3c88fd4b73942fc8f35bc4eacfd7e6bc20dc
Angelo Mantellini <manangel@cisco.com>
2021-05-27 11:22:31 +0200

    clean compileAll.sh

--- compileAll.sh
+++ compileAll.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -e
-export BUILD_HPROXY=1
-export HPROXY_URL=ssh://git@bitbucket-eng-gpk1.cisco.com:7999/ngl/hproxy.git
-export HPROXY_AAR_URL=ssh://git@bitbucket-eng-gpk1.cisco.com:7999/ngl/hproxy-aar.git
 export ANDROID_ARCH="arm64"
 make all VERBOSE=1 
 export ANDROID_ARCH="x86_64"
